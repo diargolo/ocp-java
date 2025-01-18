939ab163013b93c8e57cb86df0904eed098f4cba
Diego <diargo@gmail.com>
2025-01-18 17:52:45 -0300

    bloques de texto

---
+++ Chapter1/packagetest/TestTextBlocks.java
@@ -0,0 +1,20 @@
+package packagetest;
+
+class TestTextBlocks {
+	
+
+	public static void main(String[] args) {
+
+
+		System.out.println("desde main");
+
+		String bloque = """
+		un bloque
+		""";
+
+		System.out.println("bloque: " + bloque);
+
+
+	}
+
+}
--- Chapter1/packagetest/TestWrapperClasses.java
+++ Chapter1/packagetest/TestWrapperClasses.java
@@ -3,6 +3,39 @@ package packagetest;
 class TestWrapperClasses {
 	
 	public static void main(String[] args) {
+		
 		System.out.println("Desde main");
+
+
+		TestWrapperClasses test = new TestWrapperClasses();
+		test.testWrappers();
+
+
+
+	}
+
+	void testWrappers() {
+
+		// No compila
+		//boolean bool = Boolean.valueOf(1);
+		//long largo = Long.parseLong("123");
+
+		boolean bool = Boolean.valueOf("1");
+		System.out.println("bool: " + bool);
+
+		bool = Boolean.valueOf("TrUe");
+		System.out.println("bool: " + bool);
+
+		long largo = Long.parseLong("123");
+		System.out.println("largo: " + largo);
+
+		Long largo1 = Long.parseLong("123");
+		System.out.println("largo1: " + largo1);
+
+		int intPrimitive = Integer.parseInt("123");
+		System.out.println("intPrimitive: " + intPrimitive);
+
+		Integer intNotPrimitive = Integer.parseInt("123");
+		System.out.println("intNotPrimitive: " + intNotPrimitive);
 	}
 }
