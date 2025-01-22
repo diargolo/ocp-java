47244f65e8dc8728faa0a764c84bc9bae3db3e6f
Diego <diargo@gmail.com>
2025-01-22 15:58:00 -0300

    blocks

--- Chapter1/packagetest/TestTextBlocks.java
+++ Chapter1/packagetest/TestTextBlocks.java
@@ -7,12 +7,22 @@ class TestTextBlocks {
 
 
 		System.out.println("desde main");
+		System.out.println("*******");
+		int a = 10;
 
-		String bloque = """
-		un bloque
+		String block1 = """
+		linea 1 \n \
+		 linea 2 + a +
+			linea 3
 		""";
+		System.out.println("block1: " + block1);
 
-		System.out.println("bloque: " + bloque);
+
+		System.out.println("*******");
+
+		String block2 = """
+		linea 1  """;
+		System.out.println("*** block2: " + block2 + "****");
 
 
 	}
